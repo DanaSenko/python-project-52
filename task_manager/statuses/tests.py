ce5028faf2288db80459711d6a75dd719e6c3717
Dana Senko <dana.es@mail.ru>
2025-03-14 15:17:45 +0300

    added test to statuses

--- task_manager/statuses/tests.py
+++ task_manager/statuses/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Status
 
 # Create your tests here.
+class StatusTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='testuser', password='12345')
+        self.client.login(username='testuser', password='12345')
+        self.status = Status.objects.create(name='Test Status')
+
+    def test_status_list_view(self):
+        response = self.client.get(reverse('statuses:status_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Status')
+
+    def test_status_create_view(self):
+        response = self.client.post(reverse('statuses:status_create'), {'name': 'New Status'})
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Status.objects.filter(name='New Status').exists())
+
+    def test_status_update_view(self):
+        response = self.client.post(reverse('statuses:status_update', args=[self.status.pk]), {'name': 'Updated Status'})
+        self.assertEqual(response.status_code, 302)
+        self.status.refresh_from_db()
+        self.assertEqual(self.status.name, 'Updated Status')
+
+    def test_status_delete_view(self):
+        response = self.client.post(reverse('statuses:status_delete', args=[self.status.pk]))
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Status.objects.filter(pk=self.status.pk).exists())
