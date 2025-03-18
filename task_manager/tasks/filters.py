f74863c4e62a80259f0c933aa3246bc858743772
Dana Senko <dana.es@mail.ru>
2025-03-18 17:33:12 +0300

    add filteration on self tasks

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -11,7 +11,13 @@ class TaskFilter(django_filters.FilterSet):
     worker = django_filters.ModelChoiceFilter(queryset=User.objects.all(), label="Испольнитель",)
     label = django_filters.ModelMultipleChoiceFilter(queryset=Label.objects.all(),
         label="Метки",)
-
+    self_tasks = django_filters.BooleanFilter(method='filter_self_tasks', label="Только мои задачи",)
+    
     class Meta:
-        fields = ["status", "worker", "label", "author"]
+        fields = ["status", "worker", "label", "author", "self_tasks"]
 
+
+    def filter_self_tasks(self, queryset, reqeust, value):
+        if value:
+            return queryset.filter(author=self.request.user)
+        return queryset
