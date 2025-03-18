c6d867bfa12e71ae773aea64c468c0fe85b6c322
Dana Senko <dana.es@mail.ru>
2025-03-18 10:50:22 +0300

    updated TaskCreateForm with label widget

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -3,20 +3,30 @@ from .models import Task
 from task_manager.statuses.models import Status
 from task_manager.users.models import User
 from django.contrib.auth import get_user_model
+from task_manager.labels.models import Label
 
 User = get_user_model()
+
 
 class TaskCreateForm(forms.ModelForm):
     name = forms.CharField(max_length=100, label="Имя")
     description = forms.CharField(widget=forms.Textarea, label="Описание")
-    status = forms.ModelChoiceField(queryset=Status.objects.all(), label="Статус", to_field_name='name')
-    worker = forms.ModelChoiceField(queryset=User.objects.all(), label="Исполнитель", to_field_name='username')
-    point = forms.CharField(max_length=100, required=False, label="Метка")
-
+    status = forms.ModelChoiceField(
+        queryset=Status.objects.all(), label="Статус", to_field_name="name"
+    )
+    worker = forms.ModelChoiceField(
+        queryset=User.objects.all(), label="Исполнитель", to_field_name="username"
+    )
+    label = forms.ModelMultipleChoiceField(
+        queryset=Label.objects.all(),
+        widget=forms.SelectMultiple,
+        required=False,
+        label="Метка",
+    )
 
     class Meta:
         model = Task
-        fields = ["name", "description", "status", "worker", "point"]
+        fields = ["name", "description", "status", "worker", "label"]
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
