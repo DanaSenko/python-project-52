08ec9578d6fb033b449730cc6deab68b5ff3ffa8
Dana Senko <dana.es@mail.ru>
2025-04-10 13:33:43 +0300

    refactor(task_manages/views): fix line 38

--- task_manager/views.py
+++ task_manager/views.py
@@ -35,7 +35,7 @@ def test_rollbar(request):
     try:
         # Генерация тестовой ошибки
         raise ValueError("This is a test error for Rollbar")
-    except Exception as e:
+    except Exception:
         # Отправка ошибки в Rollbar
         rollbar.report_exc_info()
         return HttpResponse("An error occurred, but it's been reported to Rollbar.")
